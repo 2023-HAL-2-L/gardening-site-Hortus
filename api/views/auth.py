7099457d4f6dd2e52e13ad20fb38218c53163aba
sikigasa <km3t1114@yahoo.co.jp>
2023-12-06 03:00:21 +0900

    add: login

--- api/views/auth.py
+++ api/views/auth.py
@@ -3,31 +3,37 @@ from flask_login import (
     current_user,
     logout_user,
     login_required,
-    
+    login_user,
 )
 from werkzeug.security import generate_password_hash, check_password_hash
 from api.models.models import Account
 from api.database import db
+from api.views.form import RegistrationForm, LoginForm
 
 # from __init__ import app, login
 
-auth = Blueprint("auth", __name__, static_url_path="../../static", )
+auth = Blueprint(
+    "auth",
+    __name__,
+    static_url_path="../../static",
+)
+
 
 @auth.route("/signup/", methods=["POST"])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for("main.top"))
+    form = RegistrationForm()
     if request.method == "POST":
-        name = request.form["name"]
-        email = request.form["email"]
-        password = request.form["password"]
-        address = request.form["address"]
-        payment_method_id = request.form["payment_method_id"]
-        hash_password = generate_password_hash(password, method="sha256")
-        account = Account(name, email, hash_password, address, payment_method_id)
-        db.session.add(account)
-        db.session.commit()
-        return redirect(url_for("auth.login"))
-    else:
-        return redirect("login")
+        if form.validate_on_submit():
+            hash_password = generate_password_hash(form.password.data, method="sha256")
+            account = Account(name=form.name.data, email=form.email.data)
+            account.set_password(hash_password)
+            db.session.add(account)
+            db.session.commit()
+            flash("登録が完了しました")
+            return redirect(url_for("auth.login"))
+        return redirect(url_for("auth.login"), form=form)
 
 
 @auth.route("/login/", methods=["GET", "POST"])
@@ -38,15 +44,17 @@ def login():
         if current_user.is_authenticated:
             return redirect(url_for("main.top"))
         # forms.pyで定義するloginフォームを読み込む
-        # Todo: forms.pyを作成する
-        # form = LoginForm()
+        form = LoginForm()
+        
         if form.validate_on_submit():
-            account = Account.query.filter_by(accountId=form.account_id.data).one_or_none()
-            if account and check_password_hash(account.password, form.password.data):
-                login_user(account, remember=form.remember_me.data)
-                return redirect(url_for("main.top"))
-            else:
-                flash("ログインに失敗しました")
+            account = Account.query.filter_by(
+                email=form.email.data
+            ).one_or_none()
+            if account is None and not check_password_hash(account.password, form.password.data):
+                flash("アカウントが存在しないかemailかpasswordが間違っています。")
+            
+        login_user(account, remember=form.is_keep_login.data)
+        return redirect(url_for("main.top"))
 
 
 @auth.route("/logout/", methods=["GET"])
--- api/views/form.py
+++ api/views/form.py
@@ -1,17 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField, IntegerField, FileField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from api.models.models import Account
 
 class LoginForm(FlaskForm):
   email = StringField('Email', validators=[DataRequired(), Email()])
   password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
-
+  is_keep_login = BooleanField('Keep Login')
   submit = SubmitField("Login")
 
-class RegistrationFrom(FlaskForm):
+class RegistrationForm(FlaskForm):
   name = StringField('Name', validators=[DataRequired(), Length(min=1, max=60)])
   email = StringField('Email', validators=[DataRequired(), Email()])
   password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
   confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
   submit = SubmitField("登録")
+  
+  def validate_email(self,email):
+    mail = Account.query.filter_by(email= email.data).one_or_none()
+    if mail is not None:
+      raise ValidationError('このメールアドレスは既に登録されています。')
