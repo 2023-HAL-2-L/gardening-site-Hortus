7bfde06410849fb938c0f983c182176afb9f8fd0
sikigasa <km3t1114@yahoo.co.jp>
2024-02-15 17:00:01 +0900

    [WIP]add: link

--- api/views/diagnosis.py
+++ api/views/diagnosis.py
@@ -6,4 +6,81 @@ diagnosis = Blueprint("diagnosis", __name__)
 
 @diagnosis.route("/", methods=["GET"])
 def top():
-    return render_template("diagnosis.html")
+    return render_template("personal-diagnosis-start.html")
+
+@diagnosis.route("/1", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+
+@diagnosis.route("/2", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/3", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/4", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/5", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/6", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/7", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/8", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/9", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/10", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/11", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/12", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/13", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/14", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/15", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/16", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/17", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/18", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/19", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/20", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/21", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/22", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/23", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/24", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
+@diagnosis.route("/25", methods=["GET"])
+def top():
+    return render_template("diagnosis1.html")
