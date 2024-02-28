a25d9e148dea18301e2674054fd6a3a45e4bec4c
sikigasa <km3t1114@yahoo.co.jp>
2024-02-29 01:58:40 +0900

    fix: Uri

--- api/views/column.py
+++ api/views/column.py
@@ -8,36 +8,36 @@ column = Blueprint("column", __name__)
 @column.route("/")
 def column_top():
     columns_data = Column.query.all()
-    return render_template("column.html", columns_data=columns_data)
+    return render_template("column/column.html", columns_data=columns_data)
 
 @column.route("/new" , methods=["GET", "POST"])
 @login_required
 def new():
     if request.method == "GET":
         
-        return render_template("column.html")
+        return render_template("column/column.html")
     
     if request.method == "POST":
         
-        return render_template("column.html")
+        return render_template("column/column.html")
 
 @column.route("/<string:column_id>" , methods=["GET", "POST"])
 def column_page(column_id):
     if request.method == "GET":
         
-        return render_template("column-detail.html")
+        return render_template("column/column-detail.html")
     
     if request.method == "POST":
         
-        return render_template("column-detail.html")
+        return render_template("column/column-detail.html")
 
 @column.route("/<string:column_id>/edit" , methods=["GET", "POST"])
 @login_required
 def column_edit():
     if request.method == "GET":
         
-        return render_template("column-edit.html")
+        return render_template("column/column-edit.html")
     
     if request.method == "POST":
         
-        return render_template("column-edit.html")
+        return render_template("column/column-edit.html")
