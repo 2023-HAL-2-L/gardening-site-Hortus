f6d8b4a62ce715e67825f7ea7313b62445d8157f
sikigasa <km3t1114@yahoo.co.jp>
2024-02-29 02:11:54 +0900

    fix:uri

--- api/views/thread.py
+++ api/views/thread.py
@@ -8,27 +8,27 @@ thread = Blueprint("thread", __name__)
 @thread.route("/")
 def thread_top():
     threads_data = Thread.query.all()
-    return render_template("thread-top.html")
+    return render_template("thread/thread.html")
 
 @thread.route("/new" , methods=["GET", "POST"])
 @login_required
 def thread_new():
     if request.method == "GET":
         
-        return render_template("Bulletin-board-posting.html")
+        return render_template("thread/Bulletin-board-posting.html")
     
     if request.method == "POST":
         
-        return render_template("Bulletin-board-posting.html")
+        return render_template("thread/Bulletin-board-posting.html")
 
 
 @thread.route("/<string:thread_id>" , methods=["GET", "POST"])
 def thread_page(thread_id):
     if request.method == "GET":
         
-        return render_template("bulletin-board-writing.html")
+        return render_template("thread/bulletin-board-writing.html")
     
     if request.method == "POST":
         
-        return render_template("bulletin-board-writing.html")
+        return render_template("thread/bulletin-board-writing.html")
 
